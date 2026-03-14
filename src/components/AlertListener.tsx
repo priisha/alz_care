51a90b2aeab87e93d5cded415f31b324b46b3abe
Prisha Malla <you@example.com>
2026-03-14 13:03:27 +0545

    alert listener changes

--- src/components/AlertListener.tsx
+++ src/components/AlertListener.tsx
@@ -6,12 +6,10 @@ const AlertListener: React.FC = () => {
     const { showToast } = useToast();
 
     useEffect(() => {
-        // Subscribe globally to 'P001' (this could be dynamic based on login)
         const unsubscribe = patientService.subscribeToPatient(
             'P001',
-            () => { /* we might not care about generic updates here, handled in pages */ },
+            () => { },
             (newAlert) => {
-                // Using 'error' type to make it Red as requested
                 showToast(`${newAlert.type}: ${newAlert.severity} Severity`, 'error');
             }
         );
